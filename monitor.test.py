a3d75371dcb59c2c679df3001b5dabb948882225
MithunR-1998 <mithunsuarez@gmail.com>
2025-07-29 15:40:49 +0530

    Update monitor.test.py

--- monitor.test.py
+++ monitor.test.py
@@ -3,7 +3,7 @@ from monitor import vitals_ok
 
 
 class MonitorTest(unittest.TestCase):
-    def check_for_temp(self):
+    def test_for_temp(self):
         #within range for temp
         self.assertTrue(vitals_ok(101, 70, 98))
         self.assertTrue(vitals_ok(102, 70, 98))
@@ -12,9 +12,9 @@ class MonitorTest(unittest.TestCase):
         
         #outside rang for temp
         self.assertFalse(vitals_ok(102.1, 70, 98))
-        self.assertTrue(vitals_ok(94.9, 70, 98))
+        self.assertFalse(vitals_ok(94.9, 70, 98))
 
-    def check_for_pulse(self):
+    def test_for_pulse(self):
         #within range for pulse
         self.assertTrue(vitals_ok(97, 60, 98))
         self.assertTrue(vitals_ok(97, 61, 98))
@@ -23,15 +23,16 @@ class MonitorTest(unittest.TestCase):
         
         #outside rang for pulse
         self.assertFalse(vitals_ok(97, 59.9, 98))
-        self.assertTrue(vitals_ok(97, 100.1, 98))
+        self.assertFalse(vitals_ok(97, 100.1, 98))
 
-    def check_for_spo2(self):
+    def test_for_spo2(self):
         #within range for spo2
         self.assertTrue(vitals_ok(97, 70, 91))
         self.assertTrue(vitals_ok(97, 70, 90))
         
         #outside rang for spo2
         self.assertFalse(vitals_ok(97, 70, 89.9))
+        
 
 if __name__ == '__main__':
   unittest.main()
