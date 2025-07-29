b587106570448549f0f2709352e526cd5435a659
MithunR-1998 <mithunsuarez@gmail.com>
2025-07-29 13:26:12 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -15,17 +15,23 @@ def Print_Output(Message):
 def temp_check(temperature):
   if temperature > 102 or temperature < 95:
     Print_Output('Temperature critical!')
+    return False
+   return True 
 
 def pulse_check(pulseRate):
   if pulseRate < 60 or pulseRate > 100:
     Print_Output('Pulse Rate is out of range!')
+    return False
+  return True  
 
 def spo2_check(spo2):
   if spo2 < 90:
     Print_Output('Oxygen Saturation out of range!')
-    
+    return False
+  return True 
+
 def vitals_ok(temperature, pulseRate, spo2):
-  temp_check(temperature)
-  pulse_check(pulseRate)
-  spo2_check(spo2)
+  if temp_check(temperature) and pulse_check(pulseRate) and spo2_check(spo2):
+    return True
+  return False
 #vitals_ok(110,50,80)
