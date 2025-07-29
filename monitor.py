7dfdb4181669cb6eaf5e8d487c604a16cdf6f99a
MithunR-1998 <mithunsuarez@gmail.com>
2025-07-29 13:05:01 +0530

    Update monitor.py
    Used separate function for output message
    Replaced elif with if to ensure individual parameters are checked

--- monitor.py
+++ monitor.py
@@ -2,36 +2,21 @@
 from time import sleep
 import sys
 
+def Print_Output(Message):
+  print(Message)
+  for i in range(6):
+    print('\r* ', end='')
+    sys.stdout.flush()
+    sleep(1)
+    print('\r *', end='')
+    sys.stdout.flush()
+    sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    Print_Output('Temperature critical!')
+  if pulseRate < 60 or pulseRate > 100:
+    Print_Output('Pulse Rate is out of range!')
+  if spo2 < 90:
+    Print_Output('Oxygen Saturation out of range!')
+#vitals_ok(110,50,80)
